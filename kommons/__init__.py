2e37ab3c6882e8bb5e9520b78d27ef9790baeb9d
Björn Ricks <bjoern.ricks@gmail.com>
2013-10-12 16:32:42 +0200

    Add docstring for the kommons package module

--- kommons/__init__.py
+++ kommons/__init__.py
@@ -19,12 +19,18 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 # 02110-1301 USA
 
+""" Kommon python modules, classes and functions
+
+Kommons is a collection of python modules, classes and functions which are used
+in several projects and simplify standard recurrent tasks
+"""
+
 __version_info__ = ("0", "1dev1")
 __version__ = '.'.join(__version_info__)
 __description__ = "A python library for common classes and functions"
 __author__ = u"Björn Ricks"
-__author_email__ ="bjoern.ricks@gmail.com",
-__url__ ="http://github.com/bjoernricks/python-quilt",
-__license__ ="LGPLv2.1+",
+__author_email__ = "bjoern.ricks@gmail.com",
+__url__ = "http://github.com/bjoernricks/python-quilt",
+__license__ = "LGPLv2.1+",
 
 # vim: et sw=4 ts=4 tw=80:
